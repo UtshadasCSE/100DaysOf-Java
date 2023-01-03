e84e84b792e78646c0b5a5917d103d6acc86970b
Utsha Kumar Das <75168319+UtshadasCSE@users.noreply.github.com>
2023-01-03 13:36:29 +0600

    Add files via upload

---
+++ LoopEx.java
@@ -0,0 +1,28 @@
+package JavaPlacement;
+
+public class LoopEx {
+    public static void main(String[] args) {
+        int i=10;
+        do {
+            System.out.println("OK");
+            i--;
+        }while (i>9);
+//        int i=0;
+//        while (i<10){
+//            System.out.println(i);
+//            i++;
+//        }
+//        int j=10;
+//        while (j>0)
+//        {
+//            System.out.print(j+" ");
+//            j--;
+//        }
+//        for (int i=0;i<10;i++){
+//            System.out.print(i+" ");
+//        }
+//        for (int i=0;i<10;i++){
+//            System.out.println(i);
+//        }
+    }
+}
