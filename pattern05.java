4010d08d1d3719c2af24415ad10f7dbd4b8f04bd
Utsha Kumar Das <75168319+UtshadasCSE@users.noreply.github.com>
2023-01-06 23:05:37 +0600

    Add files via upload

---
+++ pattern05.java
@@ -0,0 +1,17 @@
+package JavaPlacement;
+
+public class pattern05 {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for (int j=1;j<=4;j++){
+                if (i==1 || j==1 || i==5 || j==4){
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
