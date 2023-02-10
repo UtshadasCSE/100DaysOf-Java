b95bfe0a3e6c518f2297fe92da7909156082c711
Utsha Kumar Das <75168319+UtshadasCSE@users.noreply.github.com>
2023-02-10 15:58:56 +0600

    Add files via upload

---
+++ problem01.java
@@ -0,0 +1,18 @@
+package CWHarry01;
+
+import java.util.Scanner;
+
+/**
+ * problem01
+ */
+public class problem01 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num1 = 20;
+        int num2 = 30;
+        int num3 = 40;
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
+    }
+
+}
