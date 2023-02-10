b71dc1c03fd14e2be1c298613918f12ddfb3d7ff
Utsha Kumar Das <75168319+UtshadasCSE@users.noreply.github.com>
2023-02-10 16:06:20 +0600

    Add files via upload

---
+++ problem02.java
@@ -0,0 +1,15 @@
+package CWHarry01;
+
+/**
+ * problem02
+ */
+public class problem02 {
+
+    public static void main(String[] args) {
+        float num1 = 20.00f;
+        float num2 = 90.00f;
+        float num3 = 100.00f;
+        float sum = num1 = num2 = num3;
+        System.out.println(sum);
+    }
+}
