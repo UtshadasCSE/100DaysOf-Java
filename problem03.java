074d96d9d806b6d62a38266294af4e986b537814
Utsha Kumar Das <75168319+UtshadasCSE@users.noreply.github.com>
2023-02-10 16:09:31 +0600

    Add files via upload

---
+++ problem03.java
@@ -0,0 +1,13 @@
+package CWHarry01;
+
+import java.util.Scanner;
+
+public class problem03 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String name;
+        System.out.println("What's your name?");
+        name = input.nextLine();
+        System.out.println("Hello " + name + ",How are you?");
+    }
+}
