a8700b9d6eceda6c2222581a5c17b70fe727b16e
Utsha Kumar Das <75168319+UtshadasCSE@users.noreply.github.com>
2023-02-10 16:12:04 +0600

    Add files via upload

---
+++ problem04.java
@@ -0,0 +1,11 @@
+package CWHarry01;
+
+import java.util.Scanner;
+
+public class problem04 {
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        System.out.println("Enter Something:");
+        System.out.println(Sc.hasNextInt());
+    }
+}
