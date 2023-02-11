f0010c1cfa6d25e43f9bf97f7510f248e2dcf1df
Utsha Kumar Das <75168319+UtshadasCSE@users.noreply.github.com>
2023-02-11 22:42:21 +0600

    Add files via upload

---
+++ strProblem01.java
@@ -0,0 +1,14 @@
+package CWHarry01;
+
+import java.util.Scanner;
+
+public class strProblem01 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String name;
+        System.out.println("Your name:");
+        name = input.nextLine();
+        System.out.println(name.toLowerCase());
+    }
+
+}
