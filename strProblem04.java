4feead6bf0d4e412d747bb5cd44fe56c2261bb20
Utsha Kumar Das <75168319+UtshadasCSE@users.noreply.github.com>
2023-02-12 15:25:18 +0600

    Add files via upload

---
+++ strProblem04.java
@@ -0,0 +1,9 @@
+package CWHarry01;
+
+public class strProblem04 {
+    public static void main(String[] args) {
+        String letter = "Hello   my  dear relatives,    I hope you all   yell";
+        System.out.println(letter.indexOf("   "));
+        System.out.println(letter.indexOf("     "));
+    }
+}
