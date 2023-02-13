a8f392f49b956dc2a45ff863eb44a099920da09c
Utsha Kumar Das <75168319+UtshadasCSE@users.noreply.github.com>
2023-02-13 21:23:19 +0600

    Add files via upload

---
+++ if_else01.java
@@ -0,0 +1,14 @@
+package CWHarry01;
+
+public class if_else01 {
+    public static void main(String[] args) {
+        int age = 17;
+        boolean condition = (age >= 18);
+        if (condition) {
+            System.out.println("You are adult");
+        } else {
+            System.out.println("Opps");
+        }
+    }
+
+}
