8422c1dd38bfe897ea90f33e317247e58c4548aa
Utsha Kumar Das <75168319+UtshadasCSE@users.noreply.github.com>
2023-02-13 21:44:57 +0600

    Add files via upload

---
+++ if_else03.java
@@ -0,0 +1,12 @@
+package CWHarry01;
+
+public class if_else03 {
+    public static void main(String[] args) {
+        boolean a = true, b = false;
+        if (a || b) {
+            System.out.println("Yesssss");
+        } else {
+            System.out.println("Noooo");
+        }
+    }
+}
