8816c4e87391602137506231f8323c5950487864
Utsha Kumar Das <75168319+UtshadasCSE@users.noreply.github.com>
2023-02-13 21:36:59 +0600

    Add files via upload

---
+++ if_else02.java
@@ -0,0 +1,14 @@
+package CWHarry01;
+
+public class if_else02 {
+    public static void main(String[] args) {
+        boolean a = true, b = true;
+        if (a && b) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+
+    }
+
+}
